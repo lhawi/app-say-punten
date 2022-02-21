a10d1c4e129862a72d974008e076f7c54c807408
lhawi <lukmanhawi@gmail.com>
2022-02-21 15:18:05 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lhawi/app-say-punten
+
+go 1.17
+
+require github.com/lhawi/go-say-punten v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_punten "github.com/lhawi/go-say-punten"
+)
+
+func main(){
+	fmt.Println(go_say_punten.SayPunten())
+}
